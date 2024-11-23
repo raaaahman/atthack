244f9230f4b479e49791ee30c6951fcb777e2576
raaaahman <sylvain@devindetails.com>
2024-11-23 14:25:59 +0100

    refactor(App): Progressively loads the yarn scripts

--- src/contexts/DialogueContext.tsx
+++ src/contexts/DialogueContext.tsx
@@ -1,6 +1,5 @@
 import { derive } from "derive-valtio";
 import { createContext, useContext } from "react";
-import { proxy } from "valtio";
 import YarnBound from "yarn-bound";
 
 export const DialogueContext = createContext<YarnBound | null>(null);
@@ -11,19 +10,19 @@ export function useDialogue({ screen }: { screen: string }) {
   if (!dialogue)
     throw new Error("useDialogue should be used inside a DialogueContext.");
 
-  const state = proxy(dialogue);
-
   const derived = derive({
     history: (get) =>
-      get(state).history.filter((result) => result.metadata.screen === screen),
+      get(dialogue).history.filter(
+        (result) => result.metadata.screen === screen
+      ),
     currentResult: (get) => {
-      const currentResult = get(state).currentResult;
+      const currentResult = get(dialogue).currentResult;
       return currentResult?.metadata.screen === screen ? currentResult : null;
     },
   });
 
   return {
     state: derived,
-    advance: state?.advance.bind(state),
+    advance: dialogue?.advance.bind(dialogue),
   };
 }
