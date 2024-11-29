b178eb1b545178d3cb0f1d2f54d2b21f19bc2443
raaaahman <sylvain@devindetails.com>
2024-11-29 15:38:47 +0100

    feat(NotificationsCenter): Add notifcations for when a dialogue result in another screen is displayed.

---
+++ src/components/NotificationsCenter.tsx
@@ -0,0 +1,90 @@
+import { useContext, useEffect, useState } from "react";
+import { useSnapshot } from "valtio";
+import YarnBound from "yarn-bound";
+import { useNavigate } from "@tanstack/react-router";
+import clsx from "clsx";
+
+import { DialogueContext } from "@/contexts/DialogueContext";
+import { Immutable } from "@/types/Immutable";
+import { Avatar } from "@/components/Avatar";
+import { useCharacters } from "@/contexts/CharactersContext";
+import { PLAYER_ID } from "@/constants";
+import { ArrowTopRightOnSquareIcon } from "@heroicons/react/24/outline";
+import { pathname } from "@/utils";
+
+type NotificationsCenterProps = {
+  screen?: string;
+};
+
+export function NotificationsCenter({ screen }: NotificationsCenterProps) {
+  const navigate = useNavigate();
+  const state = useContext(DialogueContext);
+  if (!state) throw new Error("Dialogue has not been initialized.");
+  const snap = useSnapshot(state);
+
+  const characters = useCharacters();
+
+  const [notifcation, setNotification] =
+    useState<Immutable<YarnBound["currentResult"]>>(null);
+
+  const characterId = notifcation?.markup
+    ?.find((tag) => tag.name === "character")
+    ?.properties.name.toLocaleLowerCase();
+
+  useEffect(() => {
+    {
+      if (
+        snap.currentResult &&
+        characterId !== PLAYER_ID &&
+        "text" in snap.currentResult &&
+        snap.currentResult.metadata.screen !== screen
+      ) {
+        setNotification(snap.currentResult);
+        const tiemout = setTimeout(() => {
+          setNotification(null);
+        }, 3600);
+        return () => clearTimeout(tiemout);
+      }
+    }
+  }, [snap.currentResult]);
+
+  const route =
+    typeof snap.currentResult?.metadata.screen === "string"
+      ? pathname(snap.currentResult.metadata.screen)
+      : undefined;
+
+  return (
+    <div className="toast toast-center toast-top z-10">
+      {notifcation ? (
+        <div
+          className={clsx(
+            "alert bg-secondary text-secondary-content flex justify-around",
+            route ? "cursor-pointer" : "cursor-auto"
+          )}
+          role={route ? "link" : ""}
+          onClick={
+            route
+              ? () => {
+                  setNotification(null);
+                  navigate({ to: route });
+                }
+              : undefined
+          }
+        >
+          {characterId ? <Avatar characterId={characterId} /> : null}
+          <div className="text-info-content">
+            {characterId
+              ? characters.getName(characterId) + " sent you a message."
+              : "You received a new message."}
+          </div>
+          <span className="sr-only">Read</span>
+          <ArrowTopRightOnSquareIcon
+            title="Read"
+            role="presentation"
+            className="size-12"
+          />
+        </div>
+      ) : null}
+    </div>
+  );
+}
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,10 +1,16 @@
-import { createRootRouteWithContext, Outlet } from "@tanstack/react-router";
+import {
+  createRootRouteWithContext,
+  Outlet,
+  useLocation,
+} from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import YarnBound, { IVariablesStorage } from "yarn-bound";
 
 import { Navbar } from "@/components/Navbar";
 import { IProject } from "@/types/IProject";
 import { CharactersRegistry } from "@/service/CharactersRegistry";
+import { NotificationsCenter } from "@/components/NotificationsCenter";
+import { screenName } from "@/utils";
 
 interface RootContext {
   project: IProject | null;
@@ -18,8 +24,11 @@ export const Route = createRootRouteWithContext<RootContext>()({
 });
 
 function Component() {
+  const { pathname } = useLocation();
+
   return (
     <>
+      <NotificationsCenter screen={screenName(pathname)} />
       <header className="order-3">
         <Navbar />
       </header>
