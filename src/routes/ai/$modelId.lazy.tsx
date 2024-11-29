f6a57ca3aad87d5b4a90d14eb88ca6471b82e33d
raaaahman <sylvain@devindetails.com>
2024-11-29 17:02:08 +0100

    fix(ai): Add model name in ai route params

--- src/routes/ai/$modelId.lazy.tsx
+++ src/routes/ai/$modelId.lazy.tsx
@@ -1,21 +1,25 @@
-import { useState } from "react";
-import { createLazyFileRoute, useLoaderData } from "@tanstack/react-router";
+import {
+  createLazyFileRoute,
+  Link,
+  useLoaderData,
+  useParams,
+} from "@tanstack/react-router";
 
 import { DialogueComponent } from "@/components/dialogue/DialogueComponent";
 import { useDialogue } from "@/contexts/DialogueContext";
 import { useCharacters } from "@/contexts/CharactersContext";
 import { SCREEN_PREFIX } from "./-constants";
 
-export const Route = createLazyFileRoute("/ai/")({
+export const Route = createLazyFileRoute("/ai/$modelId")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  const availableModels = useLoaderData({ from: "/ai/" });
-  const [currentModel, setCurrentModel] = useState("flemmy");
+  const availableModels = useLoaderData({ from: "/ai/$modelId" });
+  const { modelId } = useParams({ from: "/ai/$modelId" });
 
   const { state, advance } = useDialogue({
-    screen: SCREEN_PREFIX + currentModel,
+    screen: SCREEN_PREFIX + modelId,
   });
 
   const characters = useCharacters();
@@ -25,22 +29,13 @@ function RouteComponent() {
       <div className="w-full flex justify-center bg-neutral-100 p-2">
         <details className="dropdown">
           <summary className="btn my-2 w-52 font-bold text-lg text-center border border-neutral-300">
-            {characters.getName(currentModel)}
+            {characters.getName(modelId)}
           </summary>
           {availableModels.length > 1 ? (
             <ul className="menu dropdown-content bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
               {availableModels.map((model) => (
                 <li key={model} value={model} className="">
-                  <a
-                    href="#"
-                    onClick={(event) => {
-                      event.preventDefault();
-                      setCurrentModel(model);
-                    }}
-                  >
-                    {" "}
-                    {characters.getName(model)}
-                  </a>
+                  <Link href={"/ai/" + model}>{characters.getName(model)}</Link>
                 </li>
               ))}
             </ul>
--- src/routes/ai/$modelId.tsx
+++ src/routes/ai/$modelId.tsx
@@ -1,17 +1,17 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { SCREEN_PREFIX } from "./-constants";
+import { createFileRoute } from '@tanstack/react-router'
+import { SCREEN_PREFIX } from './-constants'
 
-export const Route = createFileRoute("/ai/")({
+export const Route = createFileRoute('/ai/$modelId')({
   loader: ({ context }) =>
     context.dialogue?.history.reduce(
       (models, result) => {
-        const screen = result.metadata.screen;
-        return typeof screen === "string" &&
+        const screen = result.metadata.screen
+        return typeof screen === 'string' &&
           screen.startsWith(SCREEN_PREFIX) &&
           !models.find((model) => screen.match(model))
           ? models.concat(result.metadata.screen.slice(SCREEN_PREFIX.length))
-          : models;
+          : models
       },
-      ["flemmy"]
+      ['flemmy'],
     ) || [],
-});
+})
