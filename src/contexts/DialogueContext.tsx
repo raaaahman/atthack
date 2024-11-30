69c32f4061bc550677ec05d4de3d66cb07130358
raaaahman <sylvain@devindetails.com>
2024-11-30 10:30:24 +0100

    feat(Navbar/Contacts): Display indicators when there's a message to read

--- src/contexts/DialogueContext.tsx
+++ src/contexts/DialogueContext.tsx
@@ -4,22 +4,26 @@ import YarnBound from "yarn-bound";
 
 export const DialogueContext = createContext<YarnBound | null>(null);
 
-export function useDialogue({ screen }: { screen: string }) {
+export function useDialogue({ screen }: { screen?: string }) {
   const dialogue = useContext(DialogueContext);
 
   if (!dialogue)
     throw new Error("useDialogue should be used inside a DialogueContext.");
 
-  const derived = derive({
-    history: (get) =>
-      get(dialogue).history.filter(
-        (result) => result.metadata.screen === screen
-      ),
-    currentResult: (get) => {
-      const currentResult = get(dialogue).currentResult;
-      return currentResult?.metadata.screen === screen ? currentResult : null;
-    },
-  });
+  const derived = screen
+    ? derive({
+        history: (get) =>
+          get(dialogue).history.filter(
+            (result) => result.metadata.screen === screen
+          ),
+        currentResult: (get) => {
+          const currentResult = get(dialogue).currentResult;
+          return currentResult?.metadata.screen === screen
+            ? currentResult
+            : null;
+        },
+      })
+    : dialogue;
 
   const advance = (optionIndex?: number) => {
     if (dialogue.currentResult?.metadata.screen === screen)
