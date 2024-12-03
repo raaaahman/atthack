d3fa7f28d4625875ca0ca65408462a37142064b8
raaaahman <sylvain@devindetails.com>
2024-12-03 11:52:07 +0100

    feat(dialogue): Flush messages past a certain limit

--- src/contexts/DialogueContext.tsx
+++ src/contexts/DialogueContext.tsx
@@ -10,12 +10,16 @@ export function useDialogue({ screen }: { screen?: string }) {
   if (!dialogue)
     throw new Error("useDialogue should be used inside a DialogueContext.");
 
+  if (dialogue.history.length > 199) {
+    dialogue.history.splice(0, 100);
+  }
+
   const derived = screen
     ? derive({
-        history: (get) =>
-          get(dialogue).history.filter(
-            (result) => result.metadata.screen === screen
-          ),
+        history: (get) => {
+          const history = get(dialogue.history);
+          return history.filter((result) => result.metadata.screen === screen);
+        },
         currentResult: (get) => {
           const currentResult = get(dialogue).currentResult;
           return currentResult?.metadata.screen === screen
